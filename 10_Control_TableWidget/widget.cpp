7aaa76663c03fd7bfeb81053982477034bf703c3
madhat386 <307262079@qq.com>
2021-02-17 01:48:14 +0800

    update

--- 10_Control_TableWidget/widget.cpp
+++ 10_Control_TableWidget/widget.cpp
@@ -3,6 +3,11 @@
 
 Widget::Widget(QWidget* parent) : QWidget(parent), ui(new Ui::Widget) {
     ui->setupUi(this);
+    //QTableWidget使用
+    //告诉控件一共有多少列
+    ui->tableWidget->setColumnCount(3);
+    ui->tableWidget->setHorizontalHeaderLabels(QStringList() << "姓名" << "性别" << "年龄");
+
 }
 
 Widget::~Widget() {
