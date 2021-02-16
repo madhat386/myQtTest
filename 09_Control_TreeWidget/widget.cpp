895f673226d872edf477e13e114c604ff512d249
madhat386 <307262079@qq.com>
2021-02-17 01:22:07 +0800

    update

--- 09_Control_TreeWidget/widget.cpp
+++ 09_Control_TreeWidget/widget.cpp
@@ -4,6 +4,13 @@
 Widget::Widget(QWidget* parent) : QWidget(parent), ui(new Ui::Widget) {
     ui->setupUi(this);
 
+    //treeWidget的使用
+    //添加头
+    ui->treeWidget->setHeaderLabels(QStringList() << "英雄" << "英雄介绍");
+
+    //添加项目
+    QTreeWidgetItem* liItem = new QTreeWidgetItem(QStringList() << "力量");
+    ui->treeWidget->addTopLevelItem(liItem);
 
 
 
