fdea36898e1d221ac7566d12c02c772c6c4d7bd4
madhat386 <307262079@qq.com>
2021-02-17 02:11:26 +0800

    update

--- 10_Control_TableWidget/widget.cpp
+++ 10_Control_TableWidget/widget.cpp
@@ -5,8 +5,34 @@ Widget::Widget(QWidget* parent) : QWidget(parent), ui(new Ui::Widget) {
     ui->setupUi(this);
     //QTableWidget使用
     //告诉控件一共有多少列
-    ui->tableWidget->setColumnCount(3);
-    ui->tableWidget->setHorizontalHeaderLabels(QStringList() << "姓名" << "性别" << "年龄");
+    QStringList list;
+    list << "姓名" << "性别" << "年龄";
+    ui->tableWidget->setColumnCount(list.size());
+    //设置水平头
+    ui->tableWidget->setHorizontalHeaderLabels(list);
+    //设置行数
+    ui->tableWidget->setRowCount(5);
+    //添加内容
+    QTableWidget* table = ui->tableWidget;
+
+    //QTableWidgetItem* item = new QTableWidgetItem("亚瑟");
+    //table->setItem(0, 0, item);
+
+    QStringList nameList;
+    nameList << "亚瑟" << "张三" << "李四" << "王五" << "赵六";
+    QStringList genderList;
+    genderList << "男" << "女" << "男" << "女" << "男";
+    QStringList ageList = QStringList() << "12" << "13" << "14" << "15" << "16";
+
+
+    for (int i = 0; i < 5; ++i) {
+        int col = 0;
+        table->setItem(i, col++, new QTableWidgetItem(nameList[i]));
+        table->setItem(i, col++, new QTableWidgetItem(genderList[i]));
+        table->setItem(i, col, new QTableWidgetItem(ageList.at(i)));
+    }
+
+
 
 }
 
