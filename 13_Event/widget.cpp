714ff467690ab0c20cb05ea7e607df8a69e709a3
madhat386 <307262079@qq.com>
2021-02-18 15:33:19 +0800

    update

--- 13_Event/widget.cpp
+++ 13_Event/widget.cpp
@@ -1,13 +1,26 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QTimerEvent>
+#include <QDebug>
 
 Widget::Widget(QWidget* parent) : QWidget(parent), ui(new Ui::Widget) {
     ui->setupUi(this);
 
-
-
+    //启动定时器(1000毫秒间隔)
+    timerId1 = startTimer(1000);
+    timerId2 = startTimer(2000);
 }
 
 Widget::~Widget() {
     delete ui;
+}
+
+void Widget::timerEvent(QTimerEvent* e) {
+    static int num = 0;
+    static int num2 = 0;
+    if(e->timerId() == timerId1) {
+        ui->label_timer->setText(QString::number(++num));
+    } else if(e->timerId() == timerId2) {
+        ui->label_timer2->setText(QString::number(++num2));
+    }
 }
--- 13_Event/widget.h
+++ 13_Event/widget.h
@@ -4,19 +4,24 @@
 #include <QWidget>
 
 namespace Ui {
-class Widget;
+    class Widget;
 }
 
-class Widget : public QWidget
-{
+class Widget : public QWidget {
     Q_OBJECT
 
 public:
-    explicit Widget(QWidget *parent = nullptr);
+    explicit Widget(QWidget* parent = nullptr);
     ~Widget();
 
 private:
-    Ui::Widget *ui;
+    Ui::Widget* ui;
+
+    //定时器时间
+    void timerEvent(QTimerEvent* e);
+    //定时器的id
+    int timerId1;
+    int timerId2;
 };
 
 #endif // WIDGET_H
