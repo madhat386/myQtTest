abaefcfad146ba166d39189f87fc93bf3c2e63cf
madhat386 <307262079@qq.com>
2021-02-19 13:43:44 +0800

    update

--- 16_QFile/widget.cpp
+++ 16_QFile/widget.cpp
@@ -1,8 +1,38 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QFile>
+#include <QTextCodec>
 
 Widget::Widget(QWidget* parent) : QWidget(parent), ui(new Ui::Widget) {
     ui->setupUi(this);
+
+    //点击按钮，选取文件
+    connect(ui->pushButton, &QPushButton::clicked, [ = ]() {
+        //打开对话框
+        QString filePath = QFileDialog::getOpenFileName(this, "打开文件", "C:\\Users\\madha.KAERMORHEN\\Desktop");
+        if(filePath.isEmpty()) {
+            QMessageBox::warning(this, "警告", "打开文件失败");
+        } else {
+            //将路径放入到lineEdit中
+            ui->lineEdit->setText(filePath);
+            //读取文件
+            QFile file(filePath);
+            //指定打开方式(只读的方式)
+            file.open(QIODevice::ReadOnly);
+            //读文件
+            QByteArray arr = file.readAll();
+            //在文本编辑框中显示
+            //ui->textEdit->setText(arr);//默认支持编码格式utf-8
+
+            //读gbk编码文件
+            QTextCodec* codec = QTextCodec::codecForName("gbk");
+            ui->textEdit->setText(codec->toUnicode(arr));
+
+        }
+    });
+
 }
 
 Widget::~Widget() {
