95eba044965ba157419a40fd6704bcce3174e804
madhat386 <307262079@qq.com>
2021-02-20 16:01:10 +0800

    update

--- 17_MyQQ/dialoglist.cpp
+++ 17_MyQQ/dialoglist.cpp
@@ -7,16 +7,29 @@ DialogList::DialogList(QWidget* parent) : QWidget(parent), ui(new Ui::DialogList
     this->setWindowTitle("QQ2021");
     this->setWindowIcon(QIcon(":/images/qq.png"));
 
+    //准备头像数据
+    QList<QString>nameList;
+    nameList << "水票奇缘" << "忆梦如澜" << "北京出版人" << "Cherry" << "淡然"
+             << "娇娇girl" << "落水无痕" << "青墨暖暖" << "无语";
+
+
+    QStringList iconNameList; //图标资源列表
+    iconNameList << "spqy" << "ymrl" << "qq" << "Cherry" << "dr"
+                 << "jj" << "lswh" << "qmnn" << "wy";
+
     //设置头像
-    QToolButton* btn = new QToolButton();
-    btn->setText("张三");
-    QPixmap icon = QPixmap(":/images/ftbz.png");
-    btn->setIcon(icon);
-    btn->setIconSize(icon.size());
-    btn->setAutoRaise(true);
-    btn->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
-    //添加进如垂直布局中（page_layout）
-    ui->page_layout->addWidget(btn);
+    for (int i = 0; i < 9; ++i) {
+        QToolButton* btn = new QToolButton();
+        btn->setText(nameList[i]);
+        QPixmap icon = QPixmap(":/images/" + iconNameList[i]);
+        btn->setIcon(icon);
+        btn->setIconSize(icon.size());
+        btn->setAutoRaise(true);
+        btn->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
+        //添加进如垂直布局中（page_layout）
+        ui->page_layout->addWidget(btn);
+    }
+
 
 
 
