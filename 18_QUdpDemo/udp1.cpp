9ba8f97c60d0d48581394abef5a7624524849b5d
madhat386 <307262079@qq.com>
2021-02-20 19:56:34 +0800

    update

--- 18_QUdpDemo/udp1.cpp
+++ 18_QUdpDemo/udp1.cpp
@@ -4,6 +4,33 @@
 Udp1::Udp1(QWidget* parent) : QWidget(parent), ui(new Ui::Udp1) {
     ui->setupUi(this);
 
+    //设置默认的端口和ip（可以更改）
+    ui->myPort->setText("8888");
+    ui->toPort->setText("9999");
+    ui->toIp->setText("127.0.0.1");
+
+    //初始化套接字
+    this->udp = new QUdpSocket(this);
+    unsigned short port = static_cast<unsigned short>( ui->myPort->text().toShort());
+    udp->bind(port);
+
+    //点击按钮发送消息
+    connect(ui->sendBtn, &QPushButton::clicked, [ = ]() {
+        udp->writeDatagram(ui->inputField->toPlainText().toUtf8(),
+                           QHostAddress(ui->toIp->text()),
+                           static_cast<unsigned short>( ui->myPort->text().toShort()));
+        ui->record->append("ME:" + ui->inputField->toPlainText());
+        ui->inputField->clear();
+    });
+
+    //绑定套接字接收数据触发的事件
+    connect(udp, &QUdpSocket::readyRead, [ = ]() {
+        long long size = udp->pendingDatagramSize();
+        QByteArray array(static_cast<int>(size), 0);
+        udp->readDatagram(array.data(), size);
+        //将数据同步到聊天记录中
+        ui->record->append(array);
+    });
 
 
 
@@ -12,3 +39,4 @@ Udp1::Udp1(QWidget* parent) : QWidget(parent), ui(new Ui::Udp1) {
 Udp1::~Udp1() {
     delete ui;
 }
+
--- 18_QUdpDemo/udp1.h
+++ 18_QUdpDemo/udp1.h
@@ -2,21 +2,24 @@
 #define UDP1_H
 
 #include <QWidget>
+//引入QUdp的头文件前需要在pro文件中添加模块network
+#include <QUdpSocket>
 
 namespace Ui {
-class Udp1;
+    class Udp1;
 }
 
-class Udp1 : public QWidget
-{
+class Udp1 : public QWidget {
     Q_OBJECT
 
 public:
-    explicit Udp1(QWidget *parent = nullptr);
+    explicit Udp1(QWidget* parent = nullptr);
     ~Udp1();
+    //套接字
+    QUdpSocket* udp;
 
 private:
-    Ui::Udp1 *ui;
+    Ui::Udp1* ui;
 };
 
 #endif // UDP1_H
