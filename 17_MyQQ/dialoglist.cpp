bcb596b5e5ed31632be19b7e227d6572606f6fdd
madhat386 <307262079@qq.com>
2021-02-20 15:56:48 +0800

    update

--- 17_MyQQ/dialoglist.cpp
+++ 17_MyQQ/dialoglist.cpp
@@ -1,8 +1,23 @@
 #include "dialoglist.h"
 #include "ui_dialoglist.h"
+#include <QToolButton>
 
 DialogList::DialogList(QWidget* parent) : QWidget(parent), ui(new Ui::DialogList) {
     ui->setupUi(this);
+    this->setWindowTitle("QQ2021");
+    this->setWindowIcon(QIcon(":/images/qq.png"));
+
+    //设置头像
+    QToolButton* btn = new QToolButton();
+    btn->setText("张三");
+    QPixmap icon = QPixmap(":/images/ftbz.png");
+    btn->setIcon(icon);
+    btn->setIconSize(icon.size());
+    btn->setAutoRaise(true);
+    btn->setToolButtonStyle(Qt::ToolButtonTextBesideIcon);
+    //添加进如垂直布局中（page_layout）
+    ui->page_layout->addWidget(btn);
+
 
 
 
